9a5ab60127b1f9e4c0e0769b910b7e005574f624
Xon <635541+Xon@users.noreply.github.com>
2024-11-02 14:55:45 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/ConversationImprovements/_output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $svCountConversationLikes Count conversation message reactions to totals
+ */
+class Options
+{
+}
