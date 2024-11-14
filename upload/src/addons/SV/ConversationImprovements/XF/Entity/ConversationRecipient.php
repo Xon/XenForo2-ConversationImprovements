a65b6bdbb71252734b1ae812cad977b8ec462474
Xon <635541+Xon@users.noreply.github.com>
2024-11-14 19:10:11 +0800

    Fix triggering search re-indexing twice for XF2.3
    Fix indexing for XF2.2 would index old recipient cached values

--- upload/src/addons/SV/ConversationImprovements/XF/Entity/ConversationRecipient.php
+++ upload/src/addons/SV/ConversationImprovements/XF/Entity/ConversationRecipient.php
@@ -37,11 +37,19 @@ class ConversationRecipient extends XFCP_ConversationRecipient
 
     protected function triggerReindex()
     {
+        if (\XF::$versionId >= 2030000)
+        {
+            return;
+        }
+
         $conversation = $this->Conversation;
         if ($conversation === null || !$conversation->hasBehavior('XF:IndexableContainer'))
         {
             return;
         }
+        $conversation->clearCache('recipient_user_ids');
+        $conversation->clearCache('active_recipient_user_ids');
+
         /** @var IndexableContainer|null $indexableContainer */
         $indexableContainer = $conversation->getBehavior('XF:IndexableContainer');
         if ($indexableContainer === null)
